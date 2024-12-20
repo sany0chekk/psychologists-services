c0373d64cfbd5b3c5fe8605ea80111d0e695fe14
Oleksandr <olexandr.veseluy@gmail.com>
2024-12-20 20:39:59 +0200

    change page animation duration and position

--- src/components/layouts/PageWrapper.tsx
+++ src/components/layouts/PageWrapper.tsx
@@ -10,11 +10,11 @@ export default function PageWrapper({ children, className = "" }: Props) {
   return (
     <motion.div
       className={className}
-      initial={{ opacity: 0, y: 50 }}
+      initial={{ opacity: 0, y: 25 }}
       animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: 50 }}
+      exit={{ opacity: 0, y: 25 }}
       transition={{
-        duration: 0.5,
+        duration: 0.3,
         ease: [0, 0.71, 0.2, 1.01],
       }}
     >
